c62fda443fbb55ac4f398ae0a4cba081ae3e96bc
Dhiraj shinde <117894068+dhirajShinde11@users.noreply.github.com>
2024-03-12 13:58:00 +0530

    Initial commit

---
+++ frontend/components/left.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const left = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default left
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import left from '../components/left'
+
+const App = () => {
+ 
+  return (
+    
+    <div className=' h-full w-full bg-black' >
+      fdgsfgs
+    <left /> 
+    </div>
+    
+  )
+}
+
+export default App
